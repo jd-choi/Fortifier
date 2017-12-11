89ccf790d05919be940496fe65c8fd8bc9ce4a24
Jaeduk Choi <jaedukchoi@gmail.com>
2017-12-10 19:45:56 -0500

    Add files via upload

--- Fortifier-Demo/ViewController.swift
+++ Fortifier-Demo/ViewController.swift
@@ -13,7 +13,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let width = UIScreen.main.bounds.size.width
+        let height = UIScreen.main.bounds.size.height
+        let imageViewBackground = UIImageView(frame: CGRect(x:0,y:0,width:width,height:height))
+        imageViewBackground.image = UIImage(named: "background")!
+        imageViewBackground.contentMode = UIViewContentMode.scaleToFill
+        self.view.addSubview(imageViewBackground)
+        self.view.sendSubview(toBack: imageViewBackground)
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
         // get current date and display in the main view
         let date = Date()
         let formatter = DateFormatter()
