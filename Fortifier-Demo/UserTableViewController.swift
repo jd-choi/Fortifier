bac8f31467e11ef25279060a4752520b30a9172d
JD Choi <jaedukchoi@gmail.com>
2017-12-13 14:30:50 -0500

    Final version

--- Fortifier-Demo/UserTableViewController.swift
+++ Fortifier-Demo/UserTableViewController.swift
@@ -23,25 +23,10 @@ class UserTableViewController: UITableViewController {
             users += savedUser
         }
         else {
-            loadSampleUsers()
+            return
         }
     }
     
-    private func loadSampleUsers() {
-        let photo1 = UIImage(named: "taurus")
-        let photo2 = UIImage(named: "capricorn")
-        let date1 = NSDate()
-        let date2 = NSDate()
-        guard let user1 = User(firstName: "JD", lastName: "Choi", gender: "Male", dob: date1,
-                               zodiac: "taurus", photo : photo1) else {
-                                fatalError("Unable to instantiate user1")
-        }
-        guard let user2 = User(firstName: "Dennis", lastName: "Park", gender: "Male", dob: date2,
-                               zodiac: "capricorn", photo : photo2) else {
-                                fatalError("Unable to instantiate user2")
-        }
-        users += [user1, user2]
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
